add2de960426f7356d27b41583c8393171240eb2
Mahesh <maheshsringeri@gmail.com>
2022-06-17 17:33:39 +0530

    After client location changes

--- src/app/admin/Components/client-locations/client-locations.component.ts
+++ src/app/admin/Components/client-locations/client-locations.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { filter } from 'rxjs';
+import { ClientLocation } from 'src/app/models/client-location';
+import { ClientLocationsService } from 'src/app/services/client-locations.service';
+import {FilterPipe} from 'src/app/pipes/filter.pipe';
 
 @Component({
   selector: 'app-client-locations',
@@ -8,10 +13,203 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class ClientLocationsComponent implements OnInit,OnDestroy
  {
 
-  constructor() { }
+  //Objects for Holding Model Data
+  clientLocations: ClientLocation[] = [];
+  
+  showLoading: boolean = true;
+
+  //Objects for Delete
+  deleteClientLocation: ClientLocation = new ClientLocation();
+  editIndex: number | any = null;
+  deleteIndex: number | any = null;
+
+  //Properties for Searching
+  searchBy: string = "clientLocationName";
+  searchText: string = "";
+
+  //Properties for Paging
+  currentPageIndex: number = 0;
+  pages: any[] = [];
+  pageSize: number = 7;
+
+  //Properties for Sorting
+  sortBy: string = "clientLocationName";
+  sortOrder: string = "ASC";
+
+  //Reactive Forms
+  newForm: FormGroup | any = null;
+  editForm: FormGroup | any = null;
+
+  @ViewChild("defaultTextBox_New") defaultTextBox_New: ElementRef | any = null;
+  @ViewChild("defaultTextBox_Edit") defaultTextBox_Edit: ElementRef | any =null;
+
+  constructor(private clientLocationsService:ClientLocationsService,private formBuilder:FormBuilder)
+  { 
+
+  }
 
   ngOnInit(): void {
+     //Get data from database
+     this.clientLocationsService .getClientLocations().subscribe(
+      (response: ClientLocation[]) => {
+        this.clientLocations = response;
+        this.showLoading = false;
+        this.calculateNoOfPages();
+      }
+     );
+
+     //Create newForm
+     this.newForm=this.formBuilder.group({
+      clientLocationID:this.formBuilder.control(null),
+      clientLocationName:this.formBuilder.control(null,[Validators.required])
+     });
+
+     //Create editForm
+     this.editForm=this.formBuilder.group({
+      clientLocationID:this.formBuilder.control(null),
+      clientLocationName:this.formBuilder.control(null,[Validators.required])
+
+     });
   }
+
+  calculateNoOfPages()
+  {
+    //Get no. of Pages
+    let filterPipe=new FilterPipe();
+    var noOfPages=Math.ceil(filterPipe.transform(this.clientLocations,this.searchBy,this.searchText).length/this.pageSize);
+    this.pages=[];
+
+    //Generate pages
+    for(let i=0;i < noOfPages;i++)
+    {
+      this.pages.push({pageIndex:i});
+    }
+
+    this.currentPageIndex=0;
+  }
+
+  onPageIndexClicked(ind:number)
+  {
+    //Set currentPageIndex
+    if(ind >= 0 && ind < this.pages.length){
+      this.currentPageIndex=ind;
+    }
+    
+    
+  }
+
+  onSearchTextChange(event:any)
+  {
+    this.calculateNoOfPages();
+    
+  }
+
+  onNewClick(event:any)
+  {
+    //reset the newForm
+    this.newForm.reset({clientLocationID:0});
+    setTimeout(() => {
+      //Focus the ClientLocation textbox in newForm
+      this.defaultTextBox_New.nativeElement.focus();
+
+    }, 100);
+
+  }
+
+  onSaveClick()
+  {
+    if(this.newForm.valid)
+    {
+       //Invoke the REST-API call
+       this.clientLocationsService.insertClientLocation(this.newForm.value).subscribe({
+         next:(response)=>{
+           //Add Response to Grid
+           var p:ClientLocation=new ClientLocation();
+           p.clientLocationID=response.clientLocationID;
+           p.clientLocationName=response.clientLocationName;
+
+           this.clientLocations.push(p);
+
+           //Reset the newForm
+           this.newForm.reset();
+           $("#newClientLocationFormCancel").trigger("click");
+           this.calculateNoOfPages();
+         },
+         error:(error)=>{
+           console.log(error);
+         }
+       });
+    }
+  }
+
+  
+  onEditClick(event:any,clientLocation:ClientLocation)
+  {
+     //Reset the editForm
+     this.editForm.reset();
+     setTimeout(() => {
+       //Set data into editForm
+       this.editForm.patchValue(clientLocation);
+       this.editIndex=this.clientLocations.indexOf(clientLocation);
+
+       //Focus the ClientLocation textbox in editForm
+       this.defaultTextBox_Edit.nativeElement.focus();
+     }, 100);
+  }
+
+  onUpdateClick()
+  {
+    if(this.editForm.valid)
+    {
+      //Invoke the REST-API call
+      this.clientLocationsService.updateClientLocation(this.editForm.value).subscribe({
+        next:(response:ClientLocation)=>{
+          //Update the response in Grid
+          this.clientLocations[this.editIndex]=response;
+
+           //Reset the editForm
+           this.editForm.reset();
+
+           $("#editClientLocationFormCancel").trigger("click");
+        },
+        error:(error)=>{
+          console.log(error)
+        }
+      });
+    }
+  }
+
+  
+  onDeleteClick(event:any,clientLocation:ClientLocation)
+  {
+     //Set data into deleteClientLocation
+     this.deleteClientLocation.clientLocationID=clientLocation.clientLocationID;
+     this.deleteClientLocation.clientLocationName=clientLocation.clientLocationName;
+     this.deleteIndex=this.clientLocations.indexOf(clientLocation);
+  }
+
+  onDeleteConfirmClick()
+  {
+    //Invoke the REST-API call
+    this.clientLocationsService.deleteClientLocation(this.deleteClientLocation.clientLocationID).subscribe({
+      next:(response)=>{
+        //Delete object in Grid
+        this.clientLocations.splice(this.deleteIndex,1);
+
+        //Clear deleteCountry
+        this.deleteClientLocation.clientLocationID=null;
+        this.deleteClientLocation.clientLocationName=null;
+
+        //Recall the calculateNoOfPages
+        this.calculateNoOfPages();
+      },
+      error:(error)=>{
+        console.log(error);
+      }
+    });
+  }
+
+  
   ngOnDestroy(): void {
     console.log("ngOnDestroy");
   }
--- src/app/services/client-locations.service.ts
+++ src/app/services/client-locations.service.ts
@@ -16,4 +16,21 @@ export class ClientLocationsService {
    {
      return this.httpClient.get<ClientLocation[]>("api/clientlocations",{responseType:"json"});
    }
+
+   
+  getClientLocationtByClientLocationID(ClientLocationID: number): Observable<ClientLocation> {
+    return this.httpClient.get<ClientLocation>("/api/clientlocations/searchbyclientlocationid/" + ClientLocationID, { responseType: "json" });
+  }
+
+  insertClientLocation(newClientLocation: ClientLocation): Observable<ClientLocation> {
+    return this.httpClient.post<ClientLocation>("/api/clientlocations", newClientLocation, { responseType: "json" });
+  }
+
+  updateClientLocation(existingClientLocation: ClientLocation): Observable<ClientLocation> {
+    return this.httpClient.put<ClientLocation>("/api/clientlocations", existingClientLocation, { responseType: "json" });
+  }
+
+  deleteClientLocation(ClientLocationID: number): Observable<string> {
+    return this.httpClient.delete<string>("/api/clientlocations?ClientLocationID=" + ClientLocationID);
+  }
 }
